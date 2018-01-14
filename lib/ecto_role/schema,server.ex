9022d2f729c9edc9362631a286bce751c2cf9e28
Jason Clark <mithereal@gmail.com>
2018-01-13 17:01:55 -0700

    add schema supervisor

---
+++ lib/ecto_role/schema,server.ex
@@ -0,0 +1,56 @@
+defmodule EctoRole.Schema.Server do
+
+  use GenServer
+
+  require Logger
+
+  @moduledoc "A Simple Server to Store Your Schemas."
+
+  alias EctoRole.Role
+
+
+  @registry_name :ecto_schema_registry
+  @name __MODULE__
+
+  defstruct fields: [ ]
+
+
+  def start_link(id) do
+
+    name = via_tuple(id)
+
+    GenServer.start_link(__MODULE__, [id], name: name)
+  end
+
+  defp via_tuple(id) do
+
+    {:via, Registry, {@registry_name, id}}
+  end
+
+
+  def init([id]) do
+
+    send(self(), { :setup, id })
+
+    state = %__MODULE__{}
+    {:ok, state }
+  end
+
+  def handle_info( { :setup, id }, state) do
+
+    updated_state = case is_nil id do
+      true -> state
+      false -> params = %{key: id}
+               record = Role.get_schema(params)
+
+               fields = []
+
+               %__MODULE__{  state | fields: fields}
+    end
+
+
+    {:noreply, updated_state}
+  end
+
+
+end
