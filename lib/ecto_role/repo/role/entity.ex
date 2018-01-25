e636b1c2dfabeb46aebe4d0e4851ce9e234d4e60
Jason Clark <mithereal@gmail.com>
2018-01-25 11:52:37 -0700

    added guards

--- lib/ecto_role/repo/role/entity.ex
+++ lib/ecto_role/repo/role/entity.ex
@@ -57,6 +57,7 @@ defmodule EctoRole.Entity do
   Fetch the Complete Entity by uuid
   """
   @spec get_entity(Map.t) :: Map.t
+
   def get_entity(%{uuid: uuid}) do
     record = Repo.get_by(Entity, uuid: uuid) |> Repo.preload(:roles)
     record
