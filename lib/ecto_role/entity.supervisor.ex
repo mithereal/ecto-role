0f067088dabf910e199778b8a19dc501c844a1eb
Jason Clark <mithereal@gmail.com>
2018-01-25 17:36:35 -0700

    .

--- lib/ecto_role/entity.supervisor.ex
+++ lib/ecto_role/entity.supervisor.ex
@@ -24,7 +24,7 @@ defmodule EctoRole.Entity.Supervisor do
   """
   def start(id) do
   ## check if is actually an entity
-    e = Repo.get_by(ENTITY, uuid: id)
+    e = Repo.get_by(ENTITY, key: id)
 
     case e do
     {:ok,_} -> Supervisor.start_child(__MODULE__, [ id ])
